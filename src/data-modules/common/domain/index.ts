42dc2812f63f85d29aedd53a6dbcb7a3100568d6
Roberto Pintos López <roberto.pintos.lopez@gmail.com>
2020-10-08 15:48:09 +0200

    feat(common): add common module

---
+++ src/data-modules/common/domain/index.ts
@@ -0,0 +1,10 @@
+import { hasValue } from "./utils/hasValue";
+import { isEmpty } from "./utils/isEmpty";
+
+// eslint-disable-next-line @typescript-eslint/typedef
+export const commonDomain = {
+  utils: {
+    hasValue,
+    isEmpty,
+  },
+};
---
+++ src/data-modules/common/domain/utils/isEmpty.ts
@@ -0,0 +1,5 @@
+import { hasValue } from "./hasValue";
+
+export function isEmpty(value: string | null | undefined): boolean {
+  return !hasValue(value) || value.trim() === "";
+}
